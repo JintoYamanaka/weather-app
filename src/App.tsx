031aa99dff9feb00a6e03cd09d24b968cbb9e722
JintoYamanaka <yamajin.ips11@gmail.com>
2023-12-10 17:28:47 +0900

    47都道府県を選択できるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,21 @@ import { useState, useEffect } from 'react';
 import { css } from '@emotion/css';
 import axios from 'axios';
 import WeatherCard from './components/WeatherCard';
+import locations from './locations.json';
 
 const appStyle = css`
   text-align: center;
 `;
 
 const App: React.FC = () => {
-  const [location, setLocation] = useState<string>('Tokyo'); // 例として東京を初期値に
+  const [location, setLocation] = useState<string>(locations[0].id);
   const [weatherData, setWeatherData] = useState<any>(null);
 
   useEffect(() => {
     const fetchWeatherData = async () => {
       try {
         const response = await axios.get(`https://weather.tsukumijima.net/api/forecast/city/${location}`);
+        console.log(response.data); // デバッグ用
         setWeatherData(response.data);
       } catch (error) {
         console.error('Weather data fetch error:', error);
@@ -28,10 +30,9 @@ const App: React.FC = () => {
     <div className={appStyle}>
       <h1>天気予報アプリ</h1>
       <select value={location} onChange={e => setLocation(e.target.value)}>
-        {/* ここに選択可能な地域のオプションを追加 */}
-        <option value="Tokyo">東京</option>
-        <option value="Osaka">大阪</option>
-        {/* 他の地域 */}
+        {locations.map((loc) => (
+          <option key={loc.id} value={loc.id}>{loc.name}</option>
+        ))}
       </select>
 
       {weatherData && weatherData.forecasts
